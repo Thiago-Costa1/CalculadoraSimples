6de3f2eceb5ace8f68fbd1dd0e445c79e283649f
Thiago <thiagocostas2017@gmail.com>
2020-11-16 17:53:55 -0300

    Completo

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Perguntar a operação
+            Console.WriteLine("Qual a operação deseja utilizar? \nSoma, Subtracao, Multiplicacao, Divisao");
+            string operacao = Console.ReadLine().ToLower();
+
+            // Pedir o 1º número
+            Console.WriteLine("Digite o 1º número :");
+            float num1 = float.Parse( Console.ReadLine() ); 
+
+            // Pedir o 2º número
+            Console.WriteLine("Digite o 2º número :");
+            float num2 = float.Parse( Console.ReadLine() );
+
+            // Fazer o calculo 
+            // F = sufixo do float
+            
+            float resultado = 0;
+            
+            switch(operacao){
+
+                case "soma":
+                    resultado = num1 + num2;
+                    break;
+
+                case "subtracao":
+                        resultado = num1 - num2;
+                        break;
+
+                case "multiplicacao":
+                    resultado = num1 * num2;
+                    break;
+                    
+                case "Divisao":
+                    resultado = num1 / num2;
+                        break;
+                
+                default:
+                    Console.WriteLine("Operação inválida! :( ");
+                        break;
+            
+            }
+
+            if (operacao != "invalida")
+               Console.WriteLine($"Calculo : {num1} com {num2} = {resultado}");
+        }
+
+    }
+}
